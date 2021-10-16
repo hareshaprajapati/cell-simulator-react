d9bf5c1208c2331ebe2cfaf958aec3bb66a5681c
haresh <hareshaprajapati@gmail.com>
2021-10-16 19:22:54 +1030

    Done with almost all UI part, creating grid and reset button working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const gridInitState = [
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0]];
+
+const App = () => {
+
+  const [grid, setGrid] = useState<number[][]>(JSON.parse(JSON.stringify(gridInitState)));
+
+  const setConfig = (rowIndex: number, columnIndex: number) => {
+    const gridCopy = JSON.parse(JSON.stringify(grid));
+    gridCopy[rowIndex][columnIndex] = gridCopy[rowIndex][columnIndex] === 0 ? 1 : 0;
+    setGrid(gridCopy);
+  }
+
+  const resetConfig = () => {
+    setGrid(JSON.parse(JSON.stringify(gridInitState)));
+  }
+
+  const generateNext = () => {
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+      <>
+        {
+          grid.map((row, rowIndex) => {
+            return <div className="row" key={rowIndex}>
+              {row.map((column, columnIndex) =>
+                <div className={` ${grid[rowIndex][columnIndex] === 1 ? 'selected-column' : ''} col `} onClick={() => setConfig(rowIndex, columnIndex)}
+                  key={`${rowIndex}+${columnIndex}`}>
+
+                </div>
+              )}
+            </div>
+          })
+        }
+      </>
+      <div className="btn-container">
+        <button className="btn next-generation-btn" onClick={generateNext}>Next Generation</button>
+        <button className="btn reset-btn" onClick={resetConfig}>Reset</button>
+      </div>
+    </div >
   );
 }
 
